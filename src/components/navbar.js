5e93dbfe1c566aa3ceaf7462455bd198fcbf4249
Nutan Singh <nutansingh159@gmail.com>
2024-06-25 21:07:38 +0530

    fixed navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -5,7 +5,8 @@ function Navbar() {
     <nav class="navbar navbar-expand-lg bg-primary" data-bs-theme='dark'>
       <div class="container">
         <Link class="navbar-brand" to="/">TodoApp</Link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarNav">

b77f5fb1dc45c3168e9516278fc79b7187965bb7
Nutan Singh <nutansingh159@gmail.com>
2024-06-25 19:50:40 +0530

    added copyright in footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <div className="container-fluid bg-secondary-subtle">
-      <p className="text-center p-4 fs-5">Todo App</p>
+      <p className="text-center p-4 fs-5">&#169; {new Date().getFullYear()} Todo App</p>
     </div>
 
   );
